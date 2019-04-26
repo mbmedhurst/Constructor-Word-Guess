266567c12c87d1aff667c727664bc227c4cd71b0
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-04-25 20:47:17 -0700

    added files and inquirer npm

---
+++ letter.js
@@ -0,0 +1,10 @@
+function Letter(character, guessed,) {
+    this.character = character
+    this.guessed = guessed
+    this.showCharacter = _ => {
+
+    }
+    this.checkGuess = (guess) => {
+
+    }
+}
